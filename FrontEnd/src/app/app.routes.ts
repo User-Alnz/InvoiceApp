604df90147ffa49588b1cc9a693f4ca253736456
User-Alnz <alexandre.anavoizard@gmail.com>
2025-08-22 16:43:22 +0200

    setup router

--- FrontEnd/src/app/app.routes.ts
+++ FrontEnd/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { CreateInvoice } from './components/create-invoice/create-invoice';
 
-export const routes: Routes = [];
+/* 
+    improve routing later => https://medium.com/@schaman762/angular-routing-best-practices-and-advanced-techniques-2da3f47226f1 
+*/
+
+export const routes: Routes = [
+    {
+        path :"",
+        component : CreateInvoice,
+        title : "create invoice"
+    }
+];
