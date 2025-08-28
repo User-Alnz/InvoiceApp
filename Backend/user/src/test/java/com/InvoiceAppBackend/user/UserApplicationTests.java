f4df22cd6df60fd31c1917bb56a6f246b8726f96
User-Alnz <alexandre.anavoizard@gmail.com>
2025-08-28 21:23:20 +0200

    user service | minimal config

---
+++ Backend/user/src/test/java/com/InvoiceAppBackend/user/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.InvoiceAppBackend.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
