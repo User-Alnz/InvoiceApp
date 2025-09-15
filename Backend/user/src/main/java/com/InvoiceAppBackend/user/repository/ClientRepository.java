495f0d255ca148484cb41885c56cae186a553dcf
User-Alnz <alexandre.anavoizard@gmail.com>
2025-09-15 11:36:33 +0200

    user-service | implement(CRUD) - method to Create, Read, Delete client from its entity

--- Backend/user/src/main/java/com/InvoiceAppBackend/user/repository/ClientRepository.java
+++ Backend/user/src/main/java/com/InvoiceAppBackend/user/repository/ClientRepository.java
@@ -1,6 +1,12 @@
 package com.InvoiceAppBackend.user.repository;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.InvoiceAppBackend.user.model.UserClient;
+import com.InvoiceAppBackend.user.model.UserCompany;
 
-public interface ClientRepository extends JpaRepository<UserClient,Long>{}
+public interface ClientRepository extends JpaRepository<UserClient,Long>
+{
+    List<UserClient> findByCompanyId(UserCompany company);
+}
