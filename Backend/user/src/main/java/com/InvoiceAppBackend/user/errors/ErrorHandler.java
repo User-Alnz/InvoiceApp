e52afb689677c317432ee569a277c8bf14100aa1
User-Alnz <alexandre.anavoizard@gmail.com>
2025-09-16 15:17:27 +0200

    user-service | add Jakarta validation over DTO and catch error on incorrect object field. Enforce guard

--- Backend/user/src/main/java/com/InvoiceAppBackend/user/errors/ErrorHandler.java
+++ Backend/user/src/main/java/com/InvoiceAppBackend/user/errors/ErrorHandler.java
@@ -1,7 +1,11 @@
 package com.InvoiceAppBackend.user.errors;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
 import com.InvoiceAppBackend.user.dto.ResponsePattern;
@@ -12,6 +16,31 @@ import jakarta.persistence.EntityNotFoundException;
 @RestControllerAdvice
 public class ErrorHandler 
 {
+    /* * *
+     *
+     * DTO validation errors
+     * 
+     * handleValidationExceptions(MethodArgumentNotValidException ex)
+     * 
+     * MethodArgumentNotValidException catches Validation errors => from OjectClass using import jakarta.validation.constraints.*; 
+     * like @NotBlank @Pattern @size etc.. 
+     * 
+     * use hashmap to write object with {"key", "value"};  
+     * 
+     * * */
+    @ExceptionHandler(MethodArgumentNotValidException.class) 
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) 
+    { 
+        Map<String, String> errors = new HashMap<>();
+
+        ex.getBindingResult().getFieldErrors()
+        .forEach(
+            error -> errors.put(error.getField(), error.getDefaultMessage())
+        );
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST); 
+    }
+
+    
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity<ResponsePattern<String>> handleNotFound(EntityNotFoundException ex) 
     {
