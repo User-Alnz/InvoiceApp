2d2faecc354466a192b4d94084f09d59aaf713c2
User-Alnz <alexandre.anavoizard@gmail.com>
2025-09-17 17:06:42 +0200

    user-service | add specific SQL querry through ORM methods

--- Backend/user/src/main/java/com/InvoiceAppBackend/user/repository/ClientRepository.java
+++ Backend/user/src/main/java/com/InvoiceAppBackend/user/repository/ClientRepository.java
@@ -8,5 +8,9 @@ import com.InvoiceAppBackend.user.model.UserClient;
 
 public interface ClientRepository extends JpaRepository<UserClient,Long>
 {
+    // SELECT * FROM company_client WHERE company_id = ?
     List<UserClient> findByCompany_Id(Long companyId);
+
+    // DELETE FROM company_client WHERE company_id = ?
+    void deleteByCompany_Id(Long companyId);
 }
