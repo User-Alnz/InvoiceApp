eb10ddbd4f6094d5f1e1ef36415e0ed64aefcd41
User-Alnz <alexandre.anavoizard@gmail.com>
2025-09-17 17:16:30 +0200

    user-service | (CRUD) get, put, delete, post on company entity

--- Backend/user/src/main/java/com/InvoiceAppBackend/user/controller/CompanyController.java
+++ Backend/user/src/main/java/com/InvoiceAppBackend/user/controller/CompanyController.java
@@ -1,11 +1,23 @@
 package com.InvoiceAppBackend.user.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import com.InvoiceAppBackend.user.service.CompanyService;
+
+import jakarta.validation.Valid;
+
 import com.InvoiceAppBackend.user.model.UserCompany;
+import com.InvoiceAppBackend.user.model.UserClient;
+import com.InvoiceAppBackend.user.dto.ResponsePattern;
+import com.InvoiceAppBackend.user.dto.request.CreateCompanyRequest;
+import com.InvoiceAppBackend.user.dto.request.UpdateCompanyRequest;
 
 import java.util.List;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
+
 
 
 @RestController
@@ -19,16 +31,43 @@ public class CompanyController
         this.service = service;
     }
 
-    @PostMapping
-    public UserCompany createCompany(@RequestBody UserCompany entity) 
+    @GetMapping("/{id}")
+    public ResponseEntity<ResponsePattern<UserCompany>> getCompanyById(@PathVariable Long id) 
     {    
-        return service.createCompany(entity);
+        UserCompany response = service.getCompanyById(id);
+
+        return ResponseEntity.ok(new ResponsePattern<>("success",200 , response));
     }
 
-    @GetMapping
-    public List<UserCompany> getAllComapny() 
+    @GetMapping("/{id}/clients")
+    public ResponseEntity<ResponsePattern<List<UserClient>>> getCompanyClients(@PathVariable Long id) 
     {
-        return service.getAllCompanies();
+        List<UserClient> response = service.getCompanyClients(id);
+
+        return ResponseEntity.ok(new ResponsePattern<>("success",200 , response));
     }
-    
+
+    @PostMapping //@Valid allows validation over Object properties CreateCompanyRequest
+    public ResponseEntity<ResponsePattern<UserCompany>> createCompany(@Valid @RequestBody CreateCompanyRequest request) 
+    {   
+        UserCompany response = service.createCompany(request);
+
+        return ResponseEntity.ok(new ResponsePattern<>("success", 200, response));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<ResponsePattern<String>> deleteCompany(@PathVariable Long id)
+    {
+        service.deleteCompany(id);
+
+        return ResponseEntity.ok(new ResponsePattern<>("success", 200, "Company deleted"));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ResponsePattern<UserCompany>> updateCompany(@PathVariable Long id, @Valid @RequestBody UpdateCompanyRequest request)
+    {
+        UserCompany response = service.updateCompany(id, request);
+
+        return ResponseEntity.ok(new ResponsePattern<>("success", 200, response));
+    }
 }
--- Backend/user/src/main/java/com/InvoiceAppBackend/user/service/CompanyService.java
+++ Backend/user/src/main/java/com/InvoiceAppBackend/user/service/CompanyService.java
@@ -1,28 +1,106 @@
 package com.InvoiceAppBackend.user.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.InvoiceAppBackend.user.dto.request.CreateCompanyRequest;
+import com.InvoiceAppBackend.user.dto.request.UpdateCompanyRequest;
+
+import com.InvoiceAppBackend.user.model.UserAccount;
+import com.InvoiceAppBackend.user.model.UserClient;
 import com.InvoiceAppBackend.user.model.UserCompany;
+import com.InvoiceAppBackend.user.repository.AccountRepository;
+import com.InvoiceAppBackend.user.repository.ClientRepository;
 import com.InvoiceAppBackend.user.repository.CompanyRepository;
+
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
 
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class CompanyService 
 {
+    private final ClientRepository clientRepository;
+    private final AccountRepository accountRepository;
     private final CompanyRepository repository;
     
-    public CompanyService(CompanyRepository repository)
+    public UserCompany getCompanyById(Long id)
     {
-        this.repository = repository;
+        return repository.findById(id)
+        .orElseThrow(()-> new EntityNotFoundException("Company not found with id :" + id));
     }
 
-    public List<UserCompany> getAllCompanies() 
+    public List<UserClient> getCompanyClients(Long id)
     {
-        return repository.findAll();
+        if(!repository.existsById(id))
+        throw new EntityNotFoundException("Company not found with id: " + id);
+
+        return clientRepository.findByCompany_Id(id);
     }
 
-    public UserCompany createCompany(UserCompany company) 
+    public UserCompany createCompany(CreateCompanyRequest request) 
     {
+        UserAccount user = accountRepository.findById(request.getUserId())
+        .orElseThrow(() -> new EntityNotFoundException("User id not found"));
+
+        long companyCount = repository.countByUserAccount_Id(user.getId());
+
+        int maxCompaniesPerUser = 3;
+
+        if(companyCount >= maxCompaniesPerUser)
+        throw new IllegalStateException("User has reached the maximum number of companies allowed. max is " + maxCompaniesPerUser + " per user");
+
+        UserCompany company = UserCompany.builder()
+        .userAccount(user) // FK to user table
+        .name(request.getName())
+        .address(request.getAddress())
+        .country(request.getCountry())
+        .postalCode(request.getPostalCode())
+        .tel(request.getTel())
+        .email(request.getEmail())
+        .legalStatus(request.getLegalStatus())
+        .shareCapital(request.getShareCapital())
+        .siren(request.getSiren())
+        .siret(request.getSiret())
+        .rcs(request.getRcs())
+        .tvaNumber(request.getTvaNumber())
+        .websiteUrl(request.getWebsiteUrl())
+        .build();
+
+        return repository.save(company);
+    }
+
+    @Transactional //-> secure deletion and rollback if one function in scope fail
+    public void deleteCompany(Long id)
+    {
+        if(!repository.existsById(id))
+        throw new EntityNotFoundException("Company not found with id: " + id);
+
+        clientRepository.deleteByCompany_Id(id); //delete client rows containing FK company_Id
+        repository.deleteById(id); //delete company row
+    }
+
+    public UserCompany updateCompany(Long id, UpdateCompanyRequest request)
+    {
+        UserCompany company = repository.findById(id)
+        .orElseThrow(() -> new EntityNotFoundException("Company id not found"));
+
+        company.setName(request.getName());
+        company.setAddress(request.getAddress());
+        company.setPostalCode(request.getPostalCode());
+        company.setCountry(request.getCountry());
+        company.setTel(request.getTel());
+        company.setEmail(request.getEmail());
+        company.setLegalStatus(request.getLegalStatus());
+        company.setShareCapital(request.getShareCapital());
+        company.setSiren(request.getSiren());
+        company.setSiret(request.getSiret());
+        company.setRcs(request.getRcs());
+        company.setTvaNumber(request.getTvaNumber());
+        company.setWebsiteUrl(request.getWebsiteUrl());
+
         return repository.save(company);
     }
 }
