9c555064e67bf33ece5ced93b7324541e1bf2e50
User-Alnz <alexandre.anavoizard@gmail.com>
2025-09-30 18:36:21 +0200

    Auth page #2 | Auth page orchestrates login-form and signup-form.

--- FrontEnd/src/app/pages/auth-page/auth-page.ts
+++ FrontEnd/src/app/pages/auth-page/auth-page.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LoginForm } from '@app/components/login-form/login-form';
+import { SignupForm } from '@app/components/signup-form/signup-form';
+
 
 @Component({
   selector: 'app-auth-page',
-  imports: [],
+  imports: [LoginForm, SignupForm, CommonModule],
   templateUrl: './auth-page.html',
   styleUrl: './auth-page.css'
 })
 export class AuthPage 
 {
-  
+
+  activeSelection: 'login' | 'signup' = 'login';
+
 }
