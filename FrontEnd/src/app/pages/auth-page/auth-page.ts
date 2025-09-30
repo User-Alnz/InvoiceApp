ba22e06c8a0856ae2783602d9e990df0a1962f9c
User-Alnz <alexandre.anavoizard@gmail.com>
2025-09-30 15:45:19 +0200

    Auth page #2 | Integration model - only CSS+html page

---
+++ FrontEnd/src/app/pages/auth-page/auth-page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-auth-page',
+  imports: [],
+  templateUrl: './auth-page.html',
+  styleUrl: './auth-page.css'
+})
+export class AuthPage 
+{
+  
+}
