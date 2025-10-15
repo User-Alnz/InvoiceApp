67cc8a16d97b3a772ca1906c30a7fa13f26bf0db
User-Alnz <alexandre.anavoizard@gmail.com>
2025-10-15 14:43:59 +0200

    feat:user-info #19 | implement ReactiveFormsModule + UI - reusable CSS style on repetitive inputs+buttons

--- FrontEnd/src/app/components/user-info/user-info.ts
+++ FrontEnd/src/app/components/user-info/user-info.ts
@@ -1,12 +1,63 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormBuilder, FormGroup  } from '@angular/forms';
 
 @Component({
   selector: 'app-user-info',
-  imports: [],
+  imports: [ReactiveFormsModule,CommonModule],
   templateUrl: './user-info.html',
   styleUrl: './user-info.css'
 })
 export class UserInfo 
 {
+  option : "Modifier" | "Cancel" = 'Modifier';
+  isAllowed : boolean = false; 
+
+
+  form : FormGroup;
+
+  constructor(private formBuilder : FormBuilder)
+  {
+    this.form = this.formBuilder.group({
+
+      name: [''],
+      address: [''],
+      postalCode: [''],
+      country: [''],
+      tel: [''],
+      email: [''],
+      legalStatus: [''],
+      shareCapital: [''],
+      siren: [''],
+      siret: [''],
+      rcs: [''],
+      tvaNumber: [''],
+      websiteUrl: ['']
+
+    })
+
+    this.form.disable();
+  }
+  
+  onSubmit()
+  {
+    console.log(this.form.value);
+  }
+
+  updateAllowed() : void
+  {
+    if(!this.isAllowed)
+    {
+      this.option = "Cancel";
+      this.form.enable();
+    }
+    else
+    {
+      this.option = "Modifier";
+      this.form.disable();
+    }
+      
+    this.isAllowed = !this.isAllowed;
+  }
 
 }
