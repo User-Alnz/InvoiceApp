56eb124c41684e77da3c211310938a32eaa245c4
User-Alnz <alexandre.anavoizard@gmail.com>
2025-10-21 13:31:44 +0200

    feat:client-view #20 | load clients in view onInit()

--- FrontEnd/src/app/components/client/client.ts
+++ FrontEnd/src/app/components/client/client.ts
@@ -1,5 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Pagination } from '../pagination/pagination';
+import { CompanyService } from '@app/services/company/companyService';
+import { ClientService } from '@app/services/client/clientService';
+import { filter, switchMap } from 'rxjs';
+import { clientList } from '@app/services/client/client.model';
 
 @Component({
   selector: 'app-client',
@@ -7,6 +11,40 @@ import { Pagination } from '../pagination/pagination';
   templateUrl: './client.html',
   styleUrl: './client.css'
 })
-export class Client {
+export class Client implements OnInit
+{
 
+  companyId?: number;
+  currentPage = 0;
+  clients:clientList[]=[];
+
+  constructor(
+    private companyService: CompanyService,
+    private clientService : ClientService)
+  {}
+
+  ngOnInit(): void 
+  {
+    this.companyService.getCompany().pipe(
+
+    filter(res => res.status ==='success'),
+    switchMap(
+      (res) =>
+      {
+        this.companyId = res.data.id;
+        return this.clientService.getClient(this.companyId, this.currentPage);
+      }
+    )
+    ).subscribe(
+    {
+      next: (res)=>
+      {
+        this.clients = res.data.content;
+      },
+      error: (err)=>
+      {
+        console.error("error caught =>",err);
+      }
+    });
+  }
 }
