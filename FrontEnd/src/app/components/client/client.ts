66a50a49871fe6d2471c1f33759674c03f81e363
User-Alnz <alexandre.anavoizard@gmail.com>
2025-10-21 09:31:57 +0200

    feat:client-view #20 | client view. Integration Html + CSS template from model

---
+++ FrontEnd/src/app/components/client/client.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Pagination } from '../pagination/pagination';
+
+@Component({
+  selector: 'app-client',
+  imports: [Pagination],
+  templateUrl: './client.html',
+  styleUrl: './client.css'
+})
+export class Client {
+
+}
